dc15b2b88f86b18ea3d6600fc6a17f6e2e856db6
Altino Alves Júnior <altinoalvesjunior@gmail.com>
2024-09-15 14:36:38 -0300

    UPD: output.info files

---
+++ projects/aiohttp/5/diff/mig5-before-test_client_session.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-		
+"""Tests for aiohttp/client.py"""
+
+import asyncio
+import contextlib
+import gc		
+import unittest		
+from unittest import mock
+
+
+import aiohttp		
+from aiohttp.client
+from aiohttp.multidict
+from aiohttp.connector
+
+
